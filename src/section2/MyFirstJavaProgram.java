196621a77fc691a0cffe42adb21bc9df9aa66f2a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-27 11:50:54 -0700

    day 3

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
-	}
+Robot moon = new Robot("batman");
+moon.penDown();	
+for(int i=0;i<4;i++) {
+moon.move(100);	
+moon.turn(90);
 }
+}
+	
+	
+	
+	}
+
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,26 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+	Robot naruto = new Robot("batman");
+	
+ 
+	
 		// 5. Set your robot's pen down 
-		
+		naruto.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		naruto.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for (int i = 0; i < 75; i++) {
+			
 		
 			// 7. Change the pen color to random
-		
+		naruto.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			naruto.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		naruto.turn(360/7);
 			// 8. Set the pen width to i
-			
+			naruto.setPenWidth(i);
+		}
 	}
 }
